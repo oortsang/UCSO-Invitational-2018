939c3752fb3fe0a7e1fc2bf416d3c557a047d64a
Jung-Sun Yi-Tsang <jung-sunyi-tsang@jung-sun-yi-tsangs-imac-2.local>
2017-12-10 17:36:47 -0600

    Initial Commit

---
+++ UCSO Invitational 2018Tests/UCSO_Invitational_2018Tests.swift
@@ -0,0 +1,36 @@
+//
+//  UCSO_Invitational_2018Tests.swift
+//  UCSO Invitational 2018Tests
+//
+//  Created by Jung-Sun Yi-Tsang on 12/10/17.
+//  Copyright © 2017 bayser. All rights reserved.
+//
+
+import XCTest
+@testable import UCSO_Invitational_2018
+
+class UCSO_Invitational_2018Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
